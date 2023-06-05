1e008175f2ae1419e45938f8816461e9ce5a3606
mvishiu11 <116217532+mvishiu11@users.noreply.github.com>
2023-06-05 09:17:29 +0200

    feat: encapsulate utils in a dedicated class

--- src/graph_utils.py
+++ src/graph_utils.py
@@ -1,6 +1,8 @@
 import copy
 
-'''
+
+class graph_utils:
+    """
     This file contains functions that are used to manipulate graphs.
 
     Functions:
@@ -9,49 +11,49 @@ import copy
         mapping(adj_matrix, pi) - returns the mapping of the graph as an adjacency matrix
         convert_edges_to_adj_matrix(edges) - converts the graph from edges to adjacency matrix
         convert_adj_matrix_to_edges(adj_matrix) - converts the graph from adjacency matrix to edges
-'''
-def get_graph_order(adj_matrix):
-    if len(adj_matrix) != len(adj_matrix[0]):
-        return -1
-    else:
-        return len(adj_matrix)
+    """
+    def get_graph_order(adj_matrix):
+        if len(adj_matrix) != len(adj_matrix[0]):
+            return -1
+        else:
+            return len(adj_matrix)
 
-def get_degree_sequence(adj_matrix):
-    degree_sequence = []
-    for vertex in range(len(adj_matrix)):
-        degree_sequence.append(sum(adj_matrix[vertex]))
-    degree_sequence.sort(reverse=True)
-    return degree_sequence
+    def get_degree_sequence(adj_matrix):
+        degree_sequence = []
+        for vertex in range(len(adj_matrix)):
+            degree_sequence.append(sum(adj_matrix[vertex]))
+        degree_sequence.sort(reverse=True)
+        return degree_sequence
 
-def mapping(adj_matrix, pi):
-    a = copy.deepcopy(adj_matrix)
-    i = 0
-    for vertice in a:
-        original = copy.deepcopy(vertice)
-        for j in range(len(vertice)):
-            vertice[j] = original[pi[j]]
-    b = copy.deepcopy(a)
-    for i in range(len(a)):
-        a[i] = b[pi[i]]
-    return a
+    def mapping(adj_matrix, pi):
+        a = copy.deepcopy(adj_matrix)
+        i = 0
+        for vertice in a:
+            original = copy.deepcopy(vertice)
+            for j in range(len(vertice)):
+                vertice[j] = original[pi[j]]
+        b = copy.deepcopy(a)
+        for i in range(len(a)):
+            a[i] = b[pi[i]]
+        return a
 
-def convert_edges_to_adj_matrix(edges):
-    adj_matrix = []
-    for i in range(len(edges)):
-        adj_matrix.append([])
-        for j in range(len(edges)):
-            if i == j:
-                adj_matrix[i].append(0)
-            elif (i, j) in edges or (j, i) in edges:
-                adj_matrix[i].append(1)
-            else:
-                adj_matrix[i].append(0)
-    return adj_matrix
+    def convert_edges_to_adj_matrix(edges):
+        adj_matrix = []
+        for i in range(len(edges)):
+            adj_matrix.append([])
+            for j in range(len(edges)):
+                if i == j:
+                    adj_matrix[i].append(0)
+                elif (i, j) in edges or (j, i) in edges:
+                    adj_matrix[i].append(1)
+                else:
+                    adj_matrix[i].append(0)
+        return adj_matrix
 
-def convert_adj_matrix_to_edges(adj_matrix):
-    edges = []
-    for i in range(len(adj_matrix)):
-        for j in range(len(adj_matrix)):
-            if adj_matrix[i][j] == 1 and (j, i) not in edges:
-                edges.append((i, j))
-    return edges
+    def convert_adj_matrix_to_edges(adj_matrix):
+        edges = []
+        for i in range(len(adj_matrix)):
+            for j in range(len(adj_matrix)):
+                if adj_matrix[i][j] == 1 and (j, i) not in edges:
+                    edges.append((i, j))
+        return edges
