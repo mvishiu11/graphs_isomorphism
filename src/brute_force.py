5b526287151943213de5e887a7a3432563800206
mvishiu11 <116217532+mvishiu11@users.noreply.github.com>
2023-06-06 12:33:33 +0200

    feat: add progress bar to calls with 10+ vertices

--- src/brute_force.py
+++ src/brute_force.py
@@ -2,6 +2,9 @@ import itertools
 import numpy as np
 from graph_utils import graph_utils as utils
 from graph import Graph
+from math import factorial
+import tqdm
+import time
 
 def brute_force_isomorphism_test(g1 : Graph, g2 : Graph) -> bool:
     """
@@ -23,9 +26,23 @@ def brute_force_isomorphism_test(g1 : Graph, g2 : Graph) -> bool:
     elif not np.array_equal(degree_sequence_1, degree_sequence_2):
         return False
     else:
-        for map in itertools.permutations([obj[0] for obj in enumerate(g1.vertices)]):
-            if(np.array_equal(utils.mapping(g1.adj_matrix, map), g2.adj_matrix)):
-                return True
-            else:
-                continue
-        return False
+        perms = itertools.permutations([obj[0] for obj in enumerate(g1.vertices)])
+        len_perms = factorial(len(g1.vertices))
+        iteration = 0
+        
+        if(len(g1.vertices) > 9):
+            for map in tqdm.tqdm(perms, total=len_perms):
+                if(np.array_equal(utils.mapping(g1.adj_matrix, map), g2.adj_matrix)):
+                    print(f"Number of iterations until convergence: {iteration}")
+                    return True
+                else:
+                    continue
+            return False
+        
+        else:
+            for map in perms:
+                if(np.array_equal(utils.mapping(g1.adj_matrix, map), g2.adj_matrix)):
+                    return True
+                else:
+                    continue
+            return False
